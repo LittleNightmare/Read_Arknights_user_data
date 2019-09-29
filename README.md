cebadf6ed2cac9cdc99e1b7aebc943c4188dc67f
LittleNightmare <littlenightmare2000@gmail.com>
2019-09-29 13:12:02 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Read_Arknights_user_data
- 
+处理明日方舟自身仓库数据，方便支持的的网页工具箱导入
+# 注意事项
+该工具的基础数据需要自行抓包
+
+**有可能**违反明日方舟相关条例
+# 使用说明
+### Prepare
+抓取登陆明日方舟的数据包，涉及到解HTTPS
+
+#### 数据包特征
+```
+之前的包:
+https://ak-gs.hypergryph.com:8443/account/login
+数据来源:
+https://ak-gs.hypergryph.com:8443/account/syncData
+```
+### First
+需要将json数据手动创建并导入 ~~复制粘贴~~ 到`arknights user data_login.json`中
+然后运行`reader.py`
+### Final
+随后会生成两个文件`ArkPlaner.json`和`lolicon.txt`
+
+可以直接通过复制粘贴的方式导入到对应网站
+# 支持网站
+可能支持其他明日方舟网页计算器
+### ArkPlaner.json
+[ArkPlaner](https://github.com/ycremar/ArkPlanner)
+### lolicon.txt
+[arknights-toolbox](https://github.com/Tsuk1ko/arknights-toolbox)
+# 鸣谢
+文件`item_table.json`来自 [明日方舟游戏数据](https://github.com/Perfare/ArknightsGameData)
+
+网页列表来自[awesome-Arknights](https://github.com/cyf-gh/awesome-Arknights)
--- reader.py
+++ reader.py
@@ -2,30 +2,46 @@ from json import load
 from base64 import b64encode
 import requests
 
-with open("arknights user data_login.json", encoding="utf-8") as f:
-    user_data = load(f)
-try:
-    with open("item_table.json", encoding="utf-8") as f:
-        check_list = load(f)
-except:
-    url = 'https://raw.githubusercontent.com/Perfare/ArknightsGameData/7c8e2fa1032b1a1b36b57aaccdc1fc29495ab133/excel/item_table.json'
-    r = requests.get(url)
-    with open("item_table.json", "wb") as code:
-        code.write(r.content)
-        check_list = load(code)
 
-inventory = user_data["user"]["inventory"]
-ArkPlaner = []
-lolicon = {"inputs": {}, "presets": []}
-for check_list_id in check_list["items"]:
-    if check_list_id in inventory:
-        content = {"name": check_list["items"][check_list_id]["name"], "need": 0, "have": inventory[check_list_id]}
-        ArkPlaner.append(content)
-        lolicon["inputs"].setdefault(check_list["items"][check_list_id]["name"],
-                                     {"need": "", "have": inventory[check_list_id]})
+def _load_file(user_date_file="arknights user data_login.json", check_list_file="item_table.json"):
+    try:
+        with open(user_date_file, "r", encoding="utf-8") as f:
+            user_data = load(f)
+    except:
+        print("文件读取出错")
 
-with open("ArkPlaner.json", "w", encoding="utf-8") as f:
-    f.write(str(ArkPlaner).replace("'", '"'))
-bse64 = b64encode(str(lolicon).replace("'", '"').encode("utf-8"))
-with open("lolicon.txt", "w")as f:
-    f.write(str(bse64, 'utf-8'))
+    try:
+        with open(check_list_file, "r", encoding="utf-8") as f:
+            check_list = load(f)
+    except:
+        url = "https://raw.github.com/Perfare/ArknightsGameData/master/excel/item_table.json"
+        r = requests.get(url)
+        with open(check_list_file, "wb") as code:
+            code.write(r.content)
+        with open(check_list_file, "r", encoding="utf-8") as f:
+            check_list = load(f)
+    return user_data, check_list
+
+
+def main(user_data, check_list):
+    inventory = user_data["user"]["inventory"]
+    ArkPlaner = []
+    lolicon = {"inputs": {}, "presets": []}
+    for check_list_id in check_list["items"]:
+        if check_list_id in inventory:
+            content = {"name": check_list["items"][check_list_id]["name"], "need": 0, "have": inventory[check_list_id]}
+            ArkPlaner.append(content)
+            lolicon["inputs"].setdefault(check_list["items"][check_list_id]["name"],
+                                         {"need": "", "have": inventory[check_list_id]})
+
+    with open("ArkPlaner.json", "w", encoding="utf-8") as f:
+        f.write(str(ArkPlaner).replace("'", '"'))
+    bse64 = b64encode(str(lolicon).replace("'", '"').encode("utf-8"))
+    with open("lolicon.txt", "w")as f:
+        f.write(str(bse64, 'utf-8'))
+
+
+if __name__ == "__main__":
+
+    user_data, check_list = _load_file()
+    main(user_data, check_list)
